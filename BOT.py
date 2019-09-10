c9214231937ce6bffe2670190957c2850383706d
syobon-arch <syobon_arch@outlook.com>
2019-09-10 21:45:14 +0900

    first

---
+++ BOT.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+# 設定
+bot = commands.Bot(command_prefix='!!')
+bot.remove_command('help')
+
+# コマンド
+@bot.command()
+async def help(ctx, tohelp='all'):
+    if tohelp == 'all':
+        embed = discord.Embed(title='現在利用可能なコマンドは以下のとおりです。', description='', color=0xffffff)
+        embed.add_field(name='s!help', value='この一覧を表示します。', inline=False)
+        await ctx.send(embed=embed)
+
+# 接続
+bot.run('NjIwOTYxMTQ0NjU5NzcxMzky.XXeaXw.Li_xIVO4nh8KtT0rlkvFskyRhoE')
